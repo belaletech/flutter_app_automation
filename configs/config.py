df361dd74f52a196fb2871b7578e426f283416e2
belaletech <belalahmadswnbhr>
2025-02-14 02:24:57 +0530

    update source code

--- configs/config.py
+++ configs/config.py
@@ -1,10 +1,11 @@
-APPIUM_SERVER = "http://belalahmad:LT_UzeFPtk5kc9Z9eQSKXiqztgW3Tkx7lUfh0phyHFXWFQjbfp@mobile-hub.lambdatest.com/wd/hub"
+APPIUM_SERVER = "https://belalahmad:LT_UzeFPtk5kc9Z9eQSKXiqztgW3Tkx7lUfh0phyHFXWFQjbfp@mobile-hub.lambdatest.com/wd/hub"
 
 CAPS = {
+    "build": "Python Appium Calculator Test",
     "platformName": "Android",
     "automationName": "flutter",
     "platformVersion": "13",
     "deviceName": "Galaxy S23+",
-    "app": "lt://APP1016045521739462030652329",  # Replace with your app's URL
+    "app": "lt://APP1016045521739462030652329",
     "isRealMobile": True
 }
--- tests/test_flutter_app.py
+++ tests/test_flutter_app.py
@@ -1,31 +1,41 @@
 import os
 from appium.webdriver import Remote
 from appium_flutter_finder.flutter_finder import FlutterElement, FlutterFinder
+from appium.options.common import AppiumOptions  # Import AppiumOptions
 from configs.config import APPIUM_SERVER, CAPS
 
-# Start Appium Driver
-driver = Remote(APPIUM_SERVER, CAPS)
+# Convert CAPS dictionary into AppiumOptions
+options = AppiumOptions()
+options.load_capabilities(CAPS)
+
+# Start Appium Driver with AppiumOptions
+driver = Remote(command_executor=APPIUM_SERVER, options=options)  # Use options instead of CAPS
 finder = FlutterFinder()
 
-# Click on the number 2
-num2_finder = driver.find_element("xpath", "//*[@content-desc='2']")
-num2_finder.click()
+# Click on the number 2 using FlutterFinder
+num2_finder = finder.by_value_key("2")  # Use `by_value_key` instead of XPath
+num2_element = FlutterElement(driver, num2_finder)
+num2_element.click()
 
 # Click on the '+' button
-plus_finder = driver.find_element("xpath", "//*[@content-desc='+']")
-plus_finder.click()
+plus_finder = finder.by_value_key("+")  
+plus_element = FlutterElement(driver, plus_finder)
+plus_element.click()
 
 # Click on the number 3
-num3_finder = driver.find_element("xpath", "//*[@content-desc='3']")
-num3_finder.click()
+num3_finder = finder.by_value_key("3")  
+num3_element = FlutterElement(driver, num3_finder)
+num3_element.click()
 
 # Click on the '=' button to get the result
-equals_finder = driver.find_element("xpath", "//*[@content-desc='=']")
-equals_finder.click()
+equals_finder = finder.by_value_key("=")  
+equals_element = FlutterElement(driver, equals_finder)
+equals_element.click()
 
 # Get the result displayed on the calculator
-result_finder = driver.find_element("xpath", "//*[@class='android.widget.TextView']")
-print("Calculation Result:", result_finder.text)
+result_finder = finder.by_value_key("result")  # Assuming your result element has a key like "result"
+result_element = FlutterElement(driver, result_finder)
+print("Calculation Result:", result_element.text)
 
 # Quit driver after execution
 driver.quit()
