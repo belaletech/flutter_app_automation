bff686a0b4f68ccdd7bcb8e251af51a08131505c
belaletech <belalahmadswnbhr@gmail.com>
2025-07-08 21:10:39 +0530

    update config file

--- configs/config.py
+++ configs/config.py
@@ -2,7 +2,11 @@ import os
 
 LT_USERNAME = os.getenv("LT_USERNAME")
 LT_ACCESS_KEY = os.getenv("LT_ACCESS_KEY")
-APP_ID = os.getenv("LAMBDATEST_APP_URL")
+APP_URL = os.getenv("LAMBDATEST_APP_URL")
+
+# Validate required env vars
+assert LT_USERNAME and LT_ACCESS_KEY, "❌ LambdaTest credentials are missing"
+assert APP_URL, "❌ LAMBDATEST_APP_URL is missing"
 
 APPIUM_SERVER = f"https://{LT_USERNAME}:{LT_ACCESS_KEY}@mobile-hub.lambdatest.com/wd/hub"
 
@@ -12,6 +16,6 @@ CAPS = {
     "automationName": "flutter",
     "platformVersion": "13",
     "deviceName": "Galaxy S23+",
-    "app": APP_ID,
+    "app": APP_URL,
     "isRealMobile": True
 }
